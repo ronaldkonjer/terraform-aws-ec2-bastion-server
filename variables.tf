eb8873c1fe15f5ec39c6f4819a983f647c115a78
Ronald Konjer <ronaldkonjer@gmail.com>
2020-06-05 10:54:18 +0200

    wip

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "namespace" {
   description = "Namespace (e.g. `eg` or `cp`)"
   type        = string
+  default     = ""
 }
 
 variable "stage" {
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
   type        = string
+  default     = ""
 }
 
 variable "environment" {
